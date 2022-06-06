d2c5bcce265c2fb18277c3c637e17d397e2093d7
CezarNeto37 <cezarmartins@msn.com>
2022-06-06 19:46:16 -0300

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { Teste2Component } from './teste2/teste2.component';
+import { TesteComponent } from './teste/teste.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Erro404Component } from './erro404/erro404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'teste2/:nome',
+    component: Teste2Component
+  },
+  {
+    path: '',
+    component: TesteComponent
+  },
+  {
+    path: '**',
+    component: Erro404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
